b6f7a3c177b5930f4e687c2e4e0448801d3765fa
unknown <sebastian.torrealba@usm.cl>
2024-12-04 23:44:29 -0300

    Changing everything, migrated to T3

--- apps/web/app/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { trpc } from "@web/app/trpc";
-
-export default async function Home() {
-  const test = await trpc.hello.query({name: "Hola"});
-  return <div>{test}</div>;
-}
