9b0ee28ce72281d1802f0d9c7cfe0c8ed21a45be
unknown <sebastian.torrealba@usm.cl>
2024-12-06 02:28:51 -0300

    Little fix of literal

--- src/app/problem/[id]/page.tsx
+++ src/app/problem/[id]/page.tsx
@@ -16,19 +16,19 @@ import CodeEditor from "~/components/codeeditor";
 
 export default function ProblemPage() {
 
-  const problem_statement: string = `
+  const problem_statement = `
   Kostya has a text $s$ consisting of $n$ words made up of Latin alphabet letters. He also has two strips on which he must write the text. The first strip can hold m characters, while the second can hold as many as needed.
 Kostya must choose a number $x$ and write the first $x$ words from s on the first strip, while all the remaining words are written on the second strip. To save space, the words are written without gaps, but each word must be entirely on one strip.
 Since space on the second strip is very valuable, Kostya asks you to choose the maximum possible number $x$ such that all words $s_1, s_2, ..., s_x$ fit on the first strip of length $m$.
 `;
 
-  const input: string = `
+  const input = `
 The first line contains an integer $t$ $(1 \\leq t \\leq 1000)$ — the number of test cases.
 The first line of each test case contains two integers $n$ and $m$ $(1 \\leq n \\leq 50; 1 \\leq m \\leq 500)$ — the number of words in the list and the maximum number of characters that can be on the first strip.
 The next $n$ lines contain one word $s_i$ of lowercase Latin letters, where the length of $s_i$ does not exceed $10$.
 `;
 
-  const output: string = `For each test case, output the maximum number of words $x$ such that the first $x$ words have a total length of no more than $m$.`
+  const output = `For each test case, output the maximum number of words $x$ such that the first $x$ words have a total length of no more than $m$.`
 
 
   const inputValue = `5
